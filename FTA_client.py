73ae26cb995ff7366717086d64a0f403e45f2acc
Joon <ychoi302@gatech.edu>
2016-11-21 08:52:19 -0500

    added a lot of error handling

--- FTA_client.py
+++ FTA_client.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import os
 from socket import *
 
 def get_args():
@@ -13,80 +14,113 @@ def get_args():
   return parser.parse_args()
 
 def main():
-  connect = input('Type in \'connect\' to connect: ')
-  if connect == 'connect':
-    try:
-      client_socket = socket(AF_INET, SOCK_STREAM)
-      client_socket.connect((server, port))
-      if debug:
-        print('The client is connected to server {0} on port {1}'.format(server,port))
-    except:
-      print ('Client was not able to connect to server, make sure your server address is correct!')
-      return
-    while True:
-      command = input('What would you like the client to do? ')
-      if debug:
-        print('[DEBUG]user input: '+command)
-      command_values = command.split()
-      possible_commands = ['disconnect', 'window', 'get', 'post', 'help']
-      if(command_values[0] in possible_commands):
+  try:
+    connect = input('Type in \'connect\' to connect: ')
+    while (connect != 'connect'):
+      print('Incorrect input format')
+      connect = input('Type in \'connect\' to connect: ')
+    if connect == 'connect':
+      try:
+        client_socket = socket(AF_INET, SOCK_STREAM)
+        client_socket.connect((server, port))
         if debug:
-            print('[DEBUG]user input is valid')
-        if command_values[0] == 'disconnect':
-          client_socket.close()
-          return
-        if command_values[0] == 'window':
-          print ('window')
-        if command_values[0] == 'get':
+          print('The client is connected to server {0} on port {1}'.format(server,port))
+      except:
+        print ('Client was not able to connect to server, make sure your server address is correct!')
+        return
+      while True:
+        command = input('What would you like the client to do? ')
+        if debug:
+          print('[DEBUG]user input: '+command)
+        command_values = command.split()
+        possible_commands = ['disconnect', 'window', 'get', 'post', 'help']
+        if(command_values[0] in possible_commands):
           if debug:
-            print('[DEBUG]get file ' + command_values[1])
-          request = 'FILERECEIVE:'+command_values[1]
-          client_socket.send(request.encode())
-          if debug:
-            print('sent content: '+request)
-          f = open('get/'+command_values[1],'wb')
-          if debug:
-            print('[DEBUG]opened file at location ' +'get/'+command_values[1] )
-          content = client_socket.recv(1024).decode()
-          while content :
+              print('[DEBUG]user input is valid')
+          if command_values[0] == 'disconnect':
+            client_socket.close()
+            return
+          if command_values[0] == 'window':
+            print ('window')
+          if command_values[0] == 'get':
             if debug:
-              print('[DEBUG]received content: '+content)
-            f.write(content.encode())
-            content = client_socket.recv(1024).decode()
-            if 'ENDPOST' in content:
-              f.close()
-              if debug:
-                print ('[DEBUG]Completed receiving {0} '.format(command_values[1]) )
-              content = None
-          if debug:
-            print('[DEBUG]File closed')
-        if command_values[0] == 'post':
-          if debug:
-            print('[DEBUG]post/upload file ' + command_values[1])
-          try:
-            f = open(command_values[1],'rb')
-            if debug:
-              print('[DEBUG]File,{0} opened'.format(command_values[1]))
-            request = 'FILESEND:'+command_values[1]
+              print('[DEBUG]get file ' + command_values[1])
+            request = 'FILERECEIVE:'+command_values[1]
             client_socket.send(request.encode())
             if debug:
-              print('[DEBUG]sent content: '+ request)
-            content = f.read(1024)
-            while (content):
-              client_socket.send(content)
-              content = f.read(1024)
+              print('sent content: '+request)
+            try:
+              filename = 'get/'+command_values[1]
+              count = 0
+              
+              while (os.path.exists(filename) == False):
+                count = count + 1
+                filename = filename + count
+
+              f = open('get/'+command_values[1],'wb')
+
               if debug:
-                print('[DEBUG]sent content: ' + content.decode())
-            f.close()
-            client_socket.send('ENDPOST'.encode())
+                print('[DEBUG]opened file at location ' +'get/'+command_values[1] )
+              
+              content = client_socket.recv(1024).decode()
+              
+              while content :
+                if debug:
+                  print('[DEBUG]received content: '+content)
+              
+                f.write(content.encode())
+                content = client_socket.recv(1024).decode()
+          
+                if 'ENDPOST' in content:
+                  f.close()
+                  if debug:
+                    print('[DEBUG]File closed')
+                    print ('[DEBUG]Completed receiving {0} '.format(command_values[1]) )
+                  content = None
+            except IOError:
+              print('could not open file')   
+              
+          if command_values[0] == 'post':
             if debug:
-              print('[DEBUG] Completed posting {0}'.format(command_values[1]))
-          except FileNotFoundError:
-            print('File does not exist')
-            client_socket.close()
-      else:
-        print('please put in your command again')
-      
+              print('[DEBUG]post/upload file ' + command_values[1])
+            try:
+              f = open(command_values[1],'rb')
+              if debug:
+                print('[DEBUG]File,{0} opened'.format(command_values[1]))
+              request = 'FILESEND:'+command_values[1]
+              client_socket.send(request.encode())
+              if debug:
+                print('[DEBUG]sent content: '+ request)
+              content = f.read(1024)
+              while (content):
+                client_socket.send(content)
+                content = f.read(1024)
+                if debug:
+                  print('[DEBUG]sent content: ' + content.decode())
+              f.close()
+              client_socket.send('ENDPOST'.encode())
+              if debug:
+                print('[DEBUG] Completed posting {0}'.format(command_values[1]))
+            except FileNotFoundError:
+              print('File does not exist')
+              client_socket.close()
+        else:
+          print('please put in your command again')
+  except:
+    print('Something went wrong / user terminated the client')
+    try:  
+      client_socket.close()
+      if debug:
+        print('[DEBUG] Closing Socket')
+    except UnboundLocalError:
+      print('[DEBUG] Socket has not been initialized')
+
+    try:
+      f.close()
+      if debug:
+        print('[DEBUG] Closing File')
+    except UnboundLocalError:
+      print('[DEBUG] File has not been initialized')
 
 
 if __name__ == '__main__':
--- FTA_server.py
+++ FTA_server.py
@@ -21,68 +21,81 @@ def get_args():
 
 
 def main():
-  #try:
-  ip_addr = '172.17.0.2'
-  server_port = port
-  server_socket = socket(AF_INET, SOCK_STREAM)
-  server_socket.bind((ip_addr, port))
-  server_socket.listen(15)
-  print('The server is running on {0} port {1}'.format(ip_addr, port))
-  while True:
-    conn_socket, addr = server_socket.accept()
-    content = conn_socket.recv(1024).decode()
-    send = False
-    while (content):
-      if send == False:
-        if "FILESEND" in content:
-          print('File will be sent from client')
-          filename = content.split(':')
-          f = open('post/'+filename[1],'wb')
-          content = conn_socket.recv(1024).decode()
-        elif "FILERECEIVE" in content:
-          print('File will be sent to the client')
-          filename = content.split(':')
-          f = open(filename[1],'rb')
-          send = True  
-        else:   
-          #Receive file from client     
-          print('inside receiving')
-          f.write(content.encode())
-          content = conn_socket.recv(1024).decode()
-          print(content)
-          if 'ENDPOST' in content:
+  try:
+    ip_addr = '172.17.0.2'
+    server_port = port
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.bind((ip_addr, port))
+    server_socket.listen(15)
+    print('The server is running on {0} port {1}'.format(ip_addr, port))
+    while True:
+      conn_socket, addr = server_socket.accept()
+      content = conn_socket.recv(1024).decode()
+      send = False
+      while (content):
+        if send == False:
+          if "FILESEND" in content:
+            print('File will be sent from client')
+            split_command = content.split(':')
+            filename = 'post/' + split_command[1]
+            f = open(filename,'wb')
+            while (os.path.exists(filename) == False):
+              count = count + 1
+              filename = filename + count
+            content = conn_socket.recv(1024).decode()
+          elif "FILERECEIVE" in content:
+            print('File will be sent to the client')
+            filename = content.split(':')
+            try:
+              f = open(filename[1],'rb')
+            except IOError:
+              print('File does not exist')
+              client_socket.close()
+            send = True  
+          else:   
+            #Receive file from client     
+            print('inside receiving')
+            f.write(content.encode())
+            content = conn_socket.recv(1024).decode()
+            print(content)
+            if 'ENDPOST' in content:
+              f.close()
+              if debug:
+                print('[DEBUG]Completed receiving {0}'.format(filename[1]))
+              content = None
+        if send:
+          #send file to client     
+          if debug:
+            print('[DEBUG]inside sending')
+          content = f.read(1024)
+          if debug:
+            print(content.decode())
+          conn_socket.send(content)
+          if 'ENDPOST' in content.decode():
             f.close()
             if debug:
-              print('[DEBUG]Completed receiving {0}'.format(filename[1]))
+              print('[DEBUG]Completed sending {0}'.format(filename[1]))
             content = None
-      if send:
-        #send file to client     
-        if debug:
-          print('[DEBUG]inside sending')
-        content = f.read(1024)
-        if debug:
-          print(content.decode())
-        conn_socket.send(content)
-        if 'ENDPOST' in content.decode():
-          f.close()
-          if debug:
-            print('[DEBUG]Completed sending {0}'.format(filename[1]))
-          content = None
-    if (send):
-      conn_socket.send('ENDPOST'.encode())
-    if(debug):
-      print('Waiting for client\'s request.')
+      if (send):
+        conn_socket.send('ENDPOST'.encode())
+      if(debug):
+        print('Waiting for client\'s request.')
 
     #if command:
       # print(command)
       #print(command)
       #print('inside command')
-  """      
+     
   except:
     print('Something went very wrong...')
-    server_socket.close()
-  """  
-
+    if server_socket in locals():
+      server_socket.close()
+      if debug:
+        print('[DEBUG] Closing Socket')
+    if f in locals():
+      f.close()
+      if debug:
+        print('[DEBUG] Closing file')
 if __name__ == '__main__':
   args = get_args()
   global port, debug
